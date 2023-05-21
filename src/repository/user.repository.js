f8101626d93ffa7df3d42d8a32ac5722b651b788
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-05-21 11:54:51 +0300

    node.js

--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -7,6 +7,11 @@ async function getAllDataUserDb() {
     return result;
 }
 
-
+async function getDataUserByIdDb(id) {
+    const client = await pool.connect();
+    const sql = 'SELECT users.id, users.name, users.surname, users_info.birth FROM users_info JOIN users ON users.info_id = users_info.id where users.id = $1';
+    const result = (await client.query(sql, [id])).rows;
+    return result;
+}
 
 module.exports = {getAllDataUserDb, getDataUserByIdDb};
