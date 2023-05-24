f5e16eba87e9f757017a90ce38877c1d13d36e35
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-05-24 20:54:17 +0300

    node.js

--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -7,7 +7,7 @@ async function getAllDataUserDb() {
   return result;
 }
 
-async function getDataUserByIdDb() {
+async function getDataUserByIdDb(id) {
   const client = await pool.connect();
   const sql =
     'SELECT users.id, users.name, users.surname, users_info.birth FROM users_info JOIN users ON users.info_id = users_info.id where users.id = $1';
@@ -17,29 +17,63 @@ async function getDataUserByIdDb() {
 
 async function createDataUserDb(name, surname, birth, city, age) {
   const client = await pool.connect();
-  const sql = 'INSERT INTO users_info (birth, city, age) values ($1, $2, $3) returning *';
-  const result = (await client.query(sql, [birth, city, age])).rows;
-  const sql_2 = 'INSERT INTO users (name, surname, info_id) values ($1, $2, $3) returning *';
-  const result_2 = (await client.query(sql_2, [name, surname, result[0].id])).rows;
-  return [{ ...result[0], ...result_2[0] }];
+  try {
+    await client.query('BEGIN');
+
+    const sql = 'INSERT INTO users_info (birth, city, age) values ($1, $2, $3) returning *';
+    const result = (await client.query(sql, [birth, city, age])).rows;
+
+    const sql_2 = 'INSERT INTO users (name, surname, info_id) values ($1, $2, $3) returning *';
+    const result_2 = (await client.query(sql_2, [name, surname, result[0].id])).rows;
+
+    await client.query('COMMIT');
+
+    return [{ ...result[0], ...result_2[0] }];
+  } catch (error) {
+    await client.query('ROLLBACK');
+
+    throw new Error(error.message);
+  }
 }
 
 async function upDataUserByIdDb(id, name, surname, birth, city, age) {
   const client = await pool.connect();
-  const sql = 'UPDATE users_info set birth = $1, city = $2, age = $3  where id = $4 returning *';
-  const result = (await client.query(sql, [birth, city, age, id])).rows;
-  const sql_2 = 'UPDATE users set name = $1, surname = $2 where info_id = $3 returning *';
-  const result_2 = (await client.query(sql_2, [name, surname, result[0].id])).rows;
-  return [{ ...result[0], ...result_2[0] }];
+  try {
+    await client.query('BEGIN');
+
+    const sql = 'UPDATE users_info set birth = $1, city = $2, age = $3 where id = $4 returning *';
+    const result = (await client.query(sql, [birth, city, age, id])).rows;
+    const sql_2 = 'UPDATE users set name = $1, surname = $2 where info_id = $3 returning *';
+    const result_2 = (await client.query(sql_2, [name, surname, result[0].id])).rows;
+
+    await client.query('COMMIT');
+
+    return [{ ...result[0], ...result_2[0] }];
+  } catch (error) {
+    await client.query('ROLLBACK');
+
+    throw new Error(error.message);
+  }
 }
 
 async function deleteUserByIdDb(id) {
   const client = await pool.connect();
-  const sql = 'delete from users_info where id = $1 returning *';
-  const result = (await client.query(sql, [id])).rows;
-  const sql_2 = 'delete from users where info_id = $1 returning *';
-  const result_2 = (await client.query(sql_2, [id])).rows;
-  return [{ ...result[0], ...result_2[0] }];
+  try {
+    await client.query('BEGIN');
+
+    const sql = 'delete from users_info where id = $1 returning *';
+    const result = (await client.query(sql, [id])).rows;
+    const sql_2 = 'delete from users where info_id = $1 returning *';
+    const result_2 = (await client.query(sql_2, [id])).rows;
+
+    await client.query('COMMIT');
+
+    return [{ ...result[0], ...result_2[0] }];
+  } catch (error) {
+    await client.query('ROLLBACK');
+
+    throw new Error(error.message);
+  }
 }
 
 module.exports = { getAllDataUserDb, getDataUserByIdDb, createDataUserDb, upDataUserByIdDb, deleteUserByIdDb };
